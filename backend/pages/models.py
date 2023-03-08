c8b8d237f34b62984636b5cedbf89a8aa4528e6f
David Oh <86807281+davidoh14@users.noreply.github.com>
2023-03-08 01:14:49 -0600

    page model setup

--- backend/pages/models.py
+++ backend/pages/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+import uuid
+
+class Page(models.Model):
+    anonymousID = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    # visitor
+    category = models.CharField(max_length=200, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    # session
+    session_id = models.CharField()
+    session_creation_time = models.DateTimeField()
+    received_at = models.DateTimeField()
+    sent_at = models.DateTimeField()
+    ip = models.GenericIPAddressField()
+    browser = models.CharField()
+    os = models.CharField()
+    # properties
+    title = models.CharField()
+    path = models.CharField()
+    url = models.URLField(max_length=200)
+    referrer = models.URLField(max_length=400)
+    search = models.CharField()
+
--- backend/websites/models.py
+++ backend/websites/models.py
@@ -3,5 +3,5 @@ from django.db import models
 from companies.models import Company
 
 class Website(models.Model):
-    hostname = models.CharField(max_length=200)
+    hostname = models.CharField(max_length=200, unique=True)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
