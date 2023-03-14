2f56d9c8c740771251a369993cde5d84e763c59d
David Oh <86807281+davidoh14@users.noreply.github.com>
2023-03-14 15:28:20 -0500

    clean

--- py_client/basic.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-endpoint = "https://localhost:8000/api/"
-
-get_response = requests.get(endpoint)
-print(get_response.json())
-
--- py_client/create.py
+++
@@ -1,7 +0,0 @@
-import requests 
-
-endpoint = "https://localhost:8000/api/pages"
-
-get_response = requests.post(endpoint)
-print(get_response.json())
-
